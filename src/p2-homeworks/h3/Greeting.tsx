3fba5c28e8a4d9183b56a413b8584910015ae5be
sandvik16 <sannndvik16@gmail.com>
2021-08-11 08:31:19 +0300

    homework 3

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -7,17 +7,18 @@ type GreetingPropsType = {
     addUser: any // need to fix any
     error: any // need to fix any
     totalUsers: any // need to fix any
+    onEnter:(e: any) => void
 }
 
 // презентационная компонента (для верстальщика)
 const Greeting: React.FC<GreetingPropsType> = (
     {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
 ) => {
-    const inputClass = s.error // need to fix with (?:)
+    const inputClass = s.error ? s.errorInput : s.input // need to fix with (?:)
 
     return (
         <div>
-            <input value={name} onChange={setNameCallback} className={inputClass}/>
+            <input value={name} onChange={setNameCallback} className={inputClass} onKeyDown={onEnter}/>
             <span>{error}</span>
             <button onClick={addUser}>add</button>
             <span>{totalUsers}</span>
